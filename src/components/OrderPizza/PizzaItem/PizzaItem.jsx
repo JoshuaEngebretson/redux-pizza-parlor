99eba98b3ad0d80c01c09f0828229f1ed07bd92c
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-05-04 13:41:02 -0500

    Conditionally rendering the add or remove button

--- src/components/OrderPizza/PizzaItem/PizzaItem.jsx
+++ src/components/OrderPizza/PizzaItem/PizzaItem.jsx
@@ -1,13 +1,38 @@
 import './PizzaItem.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 function PizzaItem({pizza}) {
+
+  const [toggleRemove, setToggleRemove] = useState(false)
+
+  const addToCart = () => {
+    console.log(`this button will add ${pizza.name} to the redux cart`);
+    setToggleRemove(true);
+  }
+
+  const removeFromCart = () => {
+    console.log(`this button will remove ${pizza.name} from the redux cart`);
+    setToggleRemove(false);
+  }
+
+  const addOrRemoveButton = () => {
+    if (toggleRemove) {
+      return <button onClick={removeFromCart}>Remove</button>
+      
+    }
+    else {
+      return <button onClick={addToCart}>Add</button>
+    }
+  }
 
   return (
     <div className="pizza-card">
       <img src={pizza.image_path}  className='square'/>
-      <h3>{pizza.name}</h3>
+      <h3 className="pizza-names">{pizza.name}</h3>
       <p className='small-text'>{pizza.description}</p>
-      <h4>{pizza.price}</h4>
+      <h4 className='card-price'>{pizza.price}</h4>
+      <p>{addOrRemoveButton()}</p>
     </div>
   )
 }
